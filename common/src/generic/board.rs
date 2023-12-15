d24ea76dd439c813957b467ace71713b5fda239a
Luis Wirth <lwirth2000@gmail.com>
2023-12-15 15:18:12 +0100

    remove superfluous `BoardTrait`

--- common/src/generic/board.rs
+++ common/src/generic/board.rs
@@ -1,5 +1,3 @@
-#![allow(clippy::let_unit_value)]
-
 use crate::{generic::line::LineState, PlayerSymbol, BOARD_AREA};
 use std::str::FromStr;
 
@@ -17,77 +15,66 @@ pub struct GenericBoard<TileType> {
   line_states: [LineState; 8],
 }
 
-// reexporting private methods that should be public
-// (needed because trait methods can't be private)
+/// public methods
 #[allow(private_bounds)]
 impl<TileType: TileTrait> GenericBoard<TileType> {
   pub fn board_state(&self) -> TileBoardState {
-    BoardTrait::board_state(self)
+    self.board_state
   }
+  fn board_state_mut(&mut self) -> &mut TileBoardState {
+    &mut self.board_state
+  }
+
+  fn line_state(&self, line: Line) -> LineState {
+    self.line_states[line.idx()]
+  }
+  fn line_state_mut(&mut self, line: Line) -> &mut LineState {
+    &mut self.line_states[line.idx()]
+  }
+
   pub fn tile(&self, pos: impl Into<Pos>) -> &TileType {
-    BoardTrait::tile(self, pos.into())
+    &self.tiles[pos.into().linear_idx()]
   }
-  pub fn trivial_tile(&self, pos_iter: impl IntoIterator<Item = Pos>) -> TrivialTile {
-    BoardTrait::trivial_tile(self, pos_iter.into_iter())
+  fn tile_mut(&mut self, pos: impl Into<Pos>) -> &mut TileType {
+    &mut self.tiles[pos.into().linear_idx()]
   }
-  pub fn could_place_symbol(&self, pos: impl IntoIterator<Item = Pos>) -> bool {
-    BoardTrait::could_place_symbol(self, pos.into_iter())
-  }
-  pub fn try_place_symbol(
-    &mut self,
-    pos: impl IntoIterator<Item = Pos>,
-    symbol: PlayerSymbol,
-  ) -> Result<(), PlaceSymbolError> {
-    BoardTrait::try_place_symbol(self, pos.into_iter(), symbol)
-  }
-}
-
-/// Trait to allow recursion on inductive board hierarchy.
-trait BoardTrait {
-  type TileType: TileTrait;
-
-  fn tile(&self, pos: Pos) -> &Self::TileType;
-  fn tile_mut(&mut self, pos: Pos) -> &mut Self::TileType;
-
-  fn board_state(&self) -> TileBoardState;
-  fn board_state_mut(&mut self) -> &mut TileBoardState;
-
-  fn line_state(&self, line: Line) -> LineState;
-  fn line_state_mut(&mut self, line: Line) -> &mut LineState;
 
   /// Returns the trivial tile at the given position, by recursively walking the board hierarchy.
-  fn trivial_tile(&self, mut pos_iter: impl Iterator<Item = Pos>) -> TrivialTile {
+  fn trivial_tile(&self, pos_iter: impl IntoIterator<Item = Pos>) -> TrivialTile {
+    let mut pos_iter = pos_iter.into_iter();
     let local_pos = pos_iter.next().expect("ran out of positions");
     TileTrait::trivial_tile(self.tile(local_pos), pos_iter)
   }
 
-  fn could_place_symbol(&self, mut pos_iter: impl Iterator<Item = Pos>) -> bool {
+  pub fn could_place_symbol(&self, pos_iter: impl IntoIterator<Item = Pos>) -> bool {
+    let mut pos_iter = pos_iter.into_iter();
     let local_pos = pos_iter.next().expect("ran out of positions");
 
     self.board_state().is_placeable()
       && TileTrait::could_place_symbol(self.tile(local_pos), pos_iter)
   }
 
-  /// Places a symbol on the given trivial tile, by recursively walking the board hierarchy and
+  /// Tries to place a symbol on the given trivial tile, by recursively walking the board hierarchy and
   /// updating the state of the `TrivialTile` and the hierarchy of super states.
-  fn try_place_symbol(
+  pub fn try_place_symbol(
     &mut self,
-    mut pos_iter: impl Iterator<Item = Pos>,
+    pos_iter: impl IntoIterator<Item = Pos>,
     symbol: PlayerSymbol,
   ) -> Result<(), PlaceSymbolError> {
+    let mut pos_iter = pos_iter.into_iter();
     let local_pos = pos_iter.next().expect("ran out of positions");
 
     if self.board_state().is_placeable() {
-      let ok = TileTrait::try_place_symbol(self.tile_mut(local_pos), pos_iter, symbol)?;
+      TileTrait::try_place_symbol(self.tile_mut(local_pos), pos_iter, symbol)?;
       self.update_super_states(local_pos);
-      Ok(ok)
+      Ok(())
     } else {
       Err(PlaceSymbolError::BoardNotPlaceable)
     }
   }
 
   /// Updates the local super states (line and board states), after a tile at the given pos has changed.
-  fn update_super_states(&mut self, local_pos: Pos) {
+  pub fn update_super_states(&mut self, local_pos: Pos) {
     for line in Line::all_through_point(local_pos) {
       let line_state = line
         .iter()
@@ -109,29 +96,33 @@ trait BoardTrait {
   }
 }
 
-/// The only implementation of `BoardTrait` is for `GenericBoard`.
-impl<TileType: TileTrait> BoardTrait for GenericBoard<TileType> {
-  type TileType = TileType;
+/// `TrivialTile` is the bottom of the tile hierarchy.
+#[derive(Debug, Clone, Copy, Default)]
+pub enum TrivialTile {
+  #[default]
+  Free,
+  Won(PlayerSymbol),
+}
 
-  fn tile(&self, pos: Pos) -> &Self::TileType {
-    &self.tiles[pos.linear_idx()]
+impl TrivialTile {
+  pub fn is_free(self) -> bool {
+    matches!(self, Self::Free)
   }
-  fn tile_mut(&mut self, pos: Pos) -> &mut Self::TileType {
-    &mut self.tiles[pos.linear_idx()]
-  }
-
-  fn board_state(&self) -> TileBoardState {
-    self.board_state
-  }
-  fn board_state_mut(&mut self) -> &mut TileBoardState {
-    &mut self.board_state
+  pub fn is_won(self) -> bool {
+    matches!(self, Self::Won(_))
   }
 
-  fn line_state(&self, line: Line) -> LineState {
-    self.line_states[line.idx()]
+  pub fn as_char(self) -> char {
+    match self {
+      Self::Free => '_',
+      Self::Won(p) => p.as_char(),
+    }
   }
-  fn line_state_mut(&mut self, line: Line) -> &mut LineState {
-    &mut self.line_states[line.idx()]
+  pub fn from_char(c: char) -> Option<Self> {
+    match c {
+      '_' => Some(Self::Free),
+      _ => PlayerSymbol::from_char(c).map(Self::Won),
+    }
   }
 }
 
@@ -179,23 +170,23 @@ impl TileTrait for TrivialTile {
 }
 
 /// Induction step of the inductive tile hierarchy.
-impl<BoardType: BoardTrait> TileTrait for BoardType {
+impl<TileType: TileTrait> TileTrait for GenericBoard<TileType> {
   fn tile_state(&self) -> TileBoardState {
     self.board_state()
   }
   fn trivial_tile(&self, pos_iter: impl Iterator<Item = Pos>) -> TrivialTile {
-    BoardTrait::trivial_tile(self, pos_iter)
+    GenericBoard::trivial_tile(self, pos_iter)
   }
 
   fn could_place_symbol(&self, pos_iter: impl Iterator<Item = Pos>) -> bool {
-    BoardTrait::could_place_symbol(self, pos_iter)
+    GenericBoard::could_place_symbol(self, pos_iter)
   }
   fn try_place_symbol(
     &mut self,
     pos_iter: impl Iterator<Item = Pos>,
     symbol: PlayerSymbol,
   ) -> Result<(), PlaceSymbolError> {
-    BoardTrait::try_place_symbol(self, pos_iter, symbol)
+    GenericBoard::try_place_symbol(self, pos_iter, symbol)
   }
 }
 
@@ -230,36 +221,6 @@ impl TileBoardState {
   }
   pub fn is_placeable(self) -> bool {
     !self.is_won() && !self.is_fully_drawn()
-  }
-}
-
-/// `TrivialTile` is the bottom of the tile hierarchy.
-#[derive(Debug, Clone, Copy, Default)]
-pub enum TrivialTile {
-  #[default]
-  Free,
-  Won(PlayerSymbol),
-}
-
-impl TrivialTile {
-  pub fn is_free(self) -> bool {
-    matches!(self, Self::Free)
-  }
-  pub fn is_won(self) -> bool {
-    matches!(self, Self::Won(_))
-  }
-
-  pub fn as_char(self) -> char {
-    match self {
-      Self::Free => '_',
-      Self::Won(p) => p.as_char(),
-    }
-  }
-  pub fn from_char(c: char) -> Option<Self> {
-    match c {
-      '_' => Some(Self::Free),
-      _ => PlayerSymbol::from_char(c).map(Self::Won),
-    }
   }
 }
 
