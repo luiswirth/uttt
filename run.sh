570032f49c3251235ee42d6ec80158cf678db984
Luis Wirth <lwirth2000@gmail.com>
2023-12-16 17:19:29 +0100

    improve `run.sh` script

--- run.sh
+++ run.sh
@@ -1,19 +1,32 @@
 #! /usr/bin/env sh
 
-server_features="auto_connect"
-client0_features="auto_connect auto_play auto_next_round"
-client1_features=$client0_features
+# feature flags
+auto_connect=true
+auto_play=true
+auto_next_round=true
 
-# kill all background processes when exiting
+server_features=""
+client_features=""
+
+[ $auto_connect = true ] && server_features="${server_features}auto_connect " && client_features="${client_features}auto_connect "
+[ $auto_play = true ] && client_features="${client_features}auto_play "
+[ $auto_next_round = true ] && client_features="${client_features}auto_next_round "
+
+cargo_cmd="cargo r"
+
+if [ "$1" = "--release" ]; then
+  cargo_cmd="cargo run --release"
+fi
+
+# kill all processes when exiting
 cleanup() {
   kill 0
 }
-
 # trap the SIGINT signal (Ctrl-C)
 trap cleanup INT
 
-cargo r --release --bin uttt-server --features "$server_features" &
-cargo r --release --bin uttt-client --features "$client0_features" &
-cargo r --release --bin uttt-client --features "$client1_features" &
+$cargo_cmd --bin uttt-server --features "$server_features" &
+$cargo_cmd --bin uttt-client --features "$client_features" &
+$cargo_cmd --bin uttt-client --features "$client_features" &
 
 wait
